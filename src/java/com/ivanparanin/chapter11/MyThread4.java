f1261ab02195439538bd895bad6d782565872c53
Ivan <ivanparanin@bk.ru>
2023-04-26 16:14:16 +0600

    edit

--- src/java/com/ivanparanin/chapter11/MyThread4.java
+++ src/java/com/ivanparanin/chapter11/MyThread4.java
@@ -1,4 +1,4 @@
-package chapter11;
+package com.ivanparanin.chapter11;
 
 class MyThread4 implements Runnable {
     Thread thrd;
@@ -24,6 +24,22 @@ class MyThread4 implements Runnable {
 }
 class MoreThreads2 {
     public static void main(String[] args) {
-        
+        System.out.println("Запуск основного потока");
+        MyThread4 mt1 = new MyThread4("Child #1");
+        MyThread4 mt2 = new MyThread4("Child #2");
+        MyThread4 mt3 = new MyThread4("Child #3");
+
+        do {
+            System.out.print(".");
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException exc) {
+                System.out.println("Прерывание основного потока");
+            }
+        }while (mt1.thrd.isAlive() ||
+                mt2.thrd.isAlive() ||
+                mt3.thrd.isAlive());
+        System.out.println("Завершение основного потока");
     }
 }
